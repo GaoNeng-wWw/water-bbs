769d07d8e77ac287d17d1da7499a85c2ac6113c7
GaoNeng-wWw <gaonenggg@outlook.com>
2026-08-10 15:10:27 +0800

    feat: category seeder

---
+++ packages/water-bbs-api/src/seeders/category-seeder.ts
@@ -0,0 +1,17 @@
+import type { EntityManager } from '@mikro-orm/core';
+import { Seeder } from '@mikro-orm/seeder';
+import { Category } from 'src/category';
+
+export class CategorySeeder extends Seeder {
+  async run(em: EntityManager): Promise<void> {
+    const dbCategory = await em.find(Category, {}, { limit: 1 });
+    if (dbCategory) {
+      return;
+    }
+    const category = em.create(Category, {
+      name: '日常',
+    });
+    em.persist(category);
+    await em.flush();
+  }
+}
