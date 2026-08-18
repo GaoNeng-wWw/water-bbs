613eece56feae81511c340967916bf829989f9d2
GaoNeng-wWw <gaonenggg@outlook.com>
2026-08-18 21:08:38 +0800

    feat: remove engine

--- packages/water-bbs-api/libs/engine/src/step/step.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { applyDecorators, Injectable, SetMetadata } from '@nestjs/common';
-import { Definition } from '../definition';
-
-export const StepKey = Symbol('step');
-
-export type StepDefinition<Param> = Definition<Param>;
-
-export const Step = (definition: StepDefinition<unknown>) =>
-  applyDecorators(Injectable(), SetMetadata(StepKey, definition));
